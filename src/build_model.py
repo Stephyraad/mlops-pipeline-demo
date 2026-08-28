b8992d9f71888d2479f319259006e533b6faa1a8
stephanieraad <stephanieraad@Mac.lan>
2026-08-28 10:31:10 -0400

    [Feature ]: Add GradientBoostingClassifier and HistGradientBoostingClassifier to the model pipeline

--- src/build_model.py
+++ src/build_model.py
@@ -1,12 +1,18 @@
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import (
+    RandomForestClassifier, 
+    GradientBoostingClassifier, 
+    HistGradientBoostingClassifier
+)
+
 
 model_dict = {
     'LogisticRegression': LogisticRegression,
     'RandomForestClassifier': RandomForestClassifier,
-    'GradientBoostingClassifier': GradientBoostingClassifier
+    'GradientBoostingClassifier': GradientBoostingClassifier,
+    'HistGradientBoostingClassifier': HistGradientBoostingClassifier
 }
 
 def build_model(preprocessor: ColumnTransformer, model_config: dict[str: any]) -> Pipeline:
