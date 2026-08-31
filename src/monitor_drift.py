61dd11f3a167501d207b065ca385ceca4eb996dd
stephanieraad <stephanieraad@Mac.lan>
2026-08-31 18:51:43 -0400

    [Feture]: Add evidently to monitor drift and generate reports

---
+++ src/monitor_drift.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from evidently import Report
+from evidently.presets import DataDriftPreset
+
+def build_drift_report(
+        reference_data: pd.DataFrame, 
+        current_data: pd.DataFrame, 
+        output_path: str
+    ) -> None:
+    report = Report(metrics=[DataDriftPreset()])
+    snapshot = report.run(reference_data=reference_data, current_data=current_data)
+    snapshot.save_html(output_path)
+
+
+def main() -> None:
+    print()
+
+if __name__ == "__main__":
+    main()
